bb2da3d12e497fae80d3b96412299402fab77759
AlexanderGura <gura_alex@list.ru>
2023-12-26 13:25:55 +0700

    Add jump and gravity.

--- game.py
+++ game.py
@@ -10,13 +10,15 @@ font = pygame.font.Font(None, 50)				# Create the font;
 # Load font, images and convert them;
 sky_surf = pygame.image.load("graphics/Sky.png").convert()
 ground_surf = pygame.image.load("graphics/Ground.png").convert()
-text_surf = font.render("Runner Game", False, "Black")
+score_surf = font.render("Score: ", False, (64, 64, 64))
+score_rect = score_surf.get_rect(midbottom = (400, 50))
 
 snail_surf = pygame.image.load("graphics/snail/snail1.png").convert_alpha()
 snail_rect = snail_surf.get_rect(midbottom = (800, 300))
 
 player_surf = pygame.image.load("graphics/Player/player_walk_1.png").convert_alpha()
 player_rect = player_surf.get_rect(midbottom = (50, 300))
+player_gravity = 0
 
 # Main game loop;
 while True:
@@ -25,15 +27,34 @@ while True:
 			pygame.quit()
 			sys.exit()
 
+		if event.type == pygame.MOUSEBUTTONDOWN:
+			if player_rect.collidepoint(event.pos) and player_rect.bottom >= 300:
+				player_gravity = -20
+
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_SPACE and player_rect.bottom >= 300:
+				player_gravity = -20
+
 	screen.blit(sky_surf, (0, 0))		# Draw sky on the window;
 	screen.blit(ground_surf, (0, 300))	# Draw ground on the window;
-	screen.blit(text_surf, (400, 50))	# Draw text on the window;
+	
+	pygame.draw.rect(screen, "#c0e8ec", score_rect)
+	pygame.draw.rect(screen, "#c0e8ec", score_rect, 10)
+	screen.blit(score_surf, score_rect)	# Draw score on the window;
+
 	snail_rect.x -= 3 					# Increase snail x position;
 	if snail_rect.right <= 0:			# Check if snail go out from the window;
 		snail_rect.left = 800
 
-	screen.blit(snail_surf, snail_rect)		# Draw snail on the window;
-	screen.blit(player_surf, player_rect)	# Draw player on the window;
+	# Snail;
+	screen.blit(snail_surf, snail_rect)
+
+	# Player;
+	player_gravity += 1
+	player_rect.y += player_gravity
+	if player_rect.bottom >= 300:
+		player_rect.bottom = 300
+	screen.blit(player_surf, player_rect)
 
 	pygame.display.update() # Update the window;
 	clock.tick(60)			# 60 frames per second;
